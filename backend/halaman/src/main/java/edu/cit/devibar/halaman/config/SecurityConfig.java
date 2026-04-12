30d96a9c39a996e17b74f0a06b5f1433fc116c6f
KennyDevz <kenneth.kenn2004@gmail.com>
2026-04-13 01:29:53 +0800

    backend(chore): update security config

--- backend/halaman/src/main/java/edu/cit/devibar/halaman/config/SecurityConfig.java
+++ backend/halaman/src/main/java/edu/cit/devibar/halaman/config/SecurityConfig.java
@@ -42,6 +42,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/uploads/**").permitAll()
+                        .requestMatchers("/api/weather/**").permitAll()
                         .requestMatchers("/api/plants/**", "/api/maintenance/**").authenticated()
                         .anyRequest().authenticated()
                 )
